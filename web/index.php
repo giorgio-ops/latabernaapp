b1c635a2df355568cebd964ef92065132073c128
Daniel Campoverde [alx741] <alx741@riseup.net>
2016-04-19 14:49:33 -0500

    Añadir prueba de hook

--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <?php
 
+echo "test";
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
